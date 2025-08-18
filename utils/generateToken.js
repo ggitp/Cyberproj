e0db16ae467057c5b444e510ad2744a96fd02505
unknown <deiroweby@gmail.com>
2025-08-18 19:58:52 +0300

    added audience to JWT exp

--- utils/generateToken.js
+++ utils/generateToken.js
@@ -4,7 +4,7 @@ const generateToken = (user) => {
   return jwt.sign(
     { email: user.email, id: user._id },
     process.env.JWT_KEY,
-    { expiresIn: '2h', issuer: 'your-app' }
+    { expiresIn: '1h', issuer: 'your-app', audience: String(user._id) }
   );
 };
 
